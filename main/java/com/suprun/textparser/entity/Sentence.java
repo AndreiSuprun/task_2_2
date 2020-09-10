d6ce8dda06358cbde49defb35790d5e9dc028e41
AndreiSuprun <66207171+AndreiSuprun@users.noreply.github.com>
2020-09-10 23:38:50 +0300

    Update Sentence.java

--- main/java/com/suprun/textparser/entity/Sentence.java
+++ main/java/com/suprun/textparser/entity/Sentence.java
@@ -1,9 +1,6 @@
 package com.suprun.textparser.entity;
 
 import java.util.List;
-import java.util.StringJoiner;
-import java.util.stream.Collector;
-import java.util.stream.Collectors;
 
 public class Sentence extends CompositePart {
 
